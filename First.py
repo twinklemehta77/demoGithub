c690513029d383fdb38710aa6fe21baf96a08456
Twinkle Mehta <er.twinklemehta@gmail.com>
2025-07-19 19:58:41 -0400

    Created a new program to add two numbers

--- First.py
+++ First.py
@@ -1,1 +1,13 @@
 print("Hello World")
+
+# Program to add two numbers
+
+# Define variables
+num1 = 5
+num2 = 8
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum)
