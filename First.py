f4d8390edaa0cdbfb60ebfc1585e995ac8844dd6
Twinkle Mehta <er.twinklemehta@gmail.com>
2025-07-19 20:02:48 -0400

    New multiplication program is added

--- First.py
+++ First.py
@@ -11,3 +11,18 @@ sum = num1 + num2
 
 # Print the result
 print("The sum of", num1, "and", num2, "is:", sum)
+
+
+# For multiplaction
+
+# Program to multiply two numbers
+
+# Define variables
+a = 6
+b = 7
+
+# Multiply the numbers
+product = a * b
+
+# Print the result
+print("The product of", a, "and", b, "is:", product)
